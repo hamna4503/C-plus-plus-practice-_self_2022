b237ae3d7e59df16dbb598def6354c1832cd8ab2
Hamna <97177481+hamna4503@users.noreply.github.com>
2022-04-15 12:34:22 +0500

    Add files via upload

---
+++ secondprog.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int a,b,c;
+    cout<<"what is a?";
+    cin>>a;
+    cout<<"what is b?";
+    cin>>b ;
+    cout<<"what is c?";
+    cin>>c ;
+    cout<<a<<"+"<<b<<"+"<<c<<" = "<<a+b+c ;
+
+    return 0 ;
+
+}
